5cdb633a16705194562590abeb84c2f9955e06d5
rubiii <me@rubiii.com>
2014-01-26 23:59:49 +0100

    added coveralls setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+unless RUBY_PLATFORM =~ /java/
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter 'spec'
+  end
+end
+
 require 'squid-rspec'
 
 RSpec.configure do |config|
